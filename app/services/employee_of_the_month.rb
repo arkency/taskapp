0b7ca811cd47454838d9043ca31256b092fbba15
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-05-14 09:37:50 +0200

    step 5

--- app/services/employee_of_the_month.rb
+++ app/services/employee_of_the_month.rb
@@ -6,16 +6,19 @@ class EmployeeOfTheMonth
                .from_all_streams
                .init(-> { { completed_tasks_to_grab: [], employee_tasks: {} } })
                .when(TaskCompleted, ->(state, event) do
-                   state[:completed_tasks_to_grab] << event.data.fetch(:task_id)
+                 state[:completed_tasks_to_grab] << event.data.fetch(:task_id)
                end)
                .when(EmployeeAssignedToTask, ->(state, event) do
                  state[:employee_tasks][event.data.fetch(:employee_id)] ||= { assigned_tasks: [], completed_tasks: 0 }
                  state[:employee_tasks][event.data.fetch(:employee_id)][:assigned_tasks] << event.data.fetch(:task_id)
                end)
+               .when(EmployeeUnassignedFromTask, ->(state, event) do
+                 state[:employee_tasks][event.data.fetch(:employee_id)][:assigned_tasks].delete(event.data.fetch(:task_id))
+               end)
                .run(event_store)
 
     result[:completed_tasks_to_grab].each do |task_id|
-      result[:employee_tasks].each do |employee_id, employee_data|
+      result[:employee_tasks].each do |_, employee_data|
         if employee_data[:assigned_tasks].include?(task_id)
           employee_data[:completed_tasks] += 1
         end
