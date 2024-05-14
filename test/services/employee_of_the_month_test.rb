4897f6bbb3dfc682c0fab1c47295436d873dda36
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-05-14 09:32:54 +0200

    step 2

--- test/services/employee_of_the_month_test.rb
+++ test/services/employee_of_the_month_test.rb
@@ -21,6 +21,25 @@ class EmployeeOfTheMonthTest < ActiveSupport::TestCase
     assert_equal 2, employee_of_the_month[:completed_tasks]
   end
 
+  def test_it_should_work_no_matter_the_order_of_events
+    task_1_id = SecureRandom.uuid
+    task_2_id = SecureRandom.uuid
+    task_3_id = SecureRandom.uuid
+
+    event_store.publish(TaskCompleted.new(data: { task_id: task_1_id }), stream_name: "Task$#{task_1_id}")
+    event_store.publish(TaskCompleted.new(data: { task_id: task_3_id }), stream_name: "Task$#{task_3_id}")
+    event_store.publish(TaskCompleted.new(data: { task_id: task_2_id }), stream_name: "Task$#{task_2_id}")
+    event_store.publish(EmployeeAssignedToTask.new(data: { task_id: task_1_id, employee_id: 1 }), stream_name: "TaskAssignments$#{task_1_id}")
+    event_store.publish(EmployeeAssignedToTask.new(data: { task_id: task_2_id, employee_id: 2 }), stream_name: "TaskAssignments$#{task_2_id}")
+    event_store.publish(EmployeeAssignedToTask.new(data: { task_id: task_3_id, employee_id: 1 }), stream_name: "TaskAssignments$#{task_3_id}")
+
+    employee_of_the_month = EmployeeOfTheMonth.new.call
+
+    assert_equal 1, employee_of_the_month[:employee_id]
+    assert_equal 2, employee_of_the_month[:completed_tasks]
+  end
+
+
   private
 
   def event_store
