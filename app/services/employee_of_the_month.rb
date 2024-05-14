5c5dbfc09b8323290ccea9cb5e1fd61602dc6618
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-05-14 09:33:14 +0200

    step 3

--- app/services/employee_of_the_month.rb
+++ app/services/employee_of_the_month.rb
@@ -6,6 +6,9 @@ class EmployeeOfTheMonth
                .from_all_streams
                .init(-> { { completed_tasks_to_grab: [], employee_tasks: {} } })
                .when(TaskCompleted, ->(state, event) do
+                 if state[:employee_tasks].empty?
+                   state[:completed_tasks_to_grab] << event.data.fetch(:task_id)
+                 end
                  state[:employee_tasks].each do |employee_id, employee_data|
                    if employee_data[:assigned_tasks].include?(event.data.fetch(:task_id))
                      employee_data[:completed_tasks] += 1
@@ -18,6 +21,13 @@ class EmployeeOfTheMonth
                end)
                .run(event_store)
 
+    result[:completed_tasks_to_grab].each do |task_id|
+      result[:employee_tasks].each do |employee_id, employee_data|
+        if employee_data[:assigned_tasks].include?(task_id)
+          employee_data[:completed_tasks] += 1
+        end
+      end
+    end
 
     employee_id, employee_data = result[:employee_tasks].max_by { |_, data| data[:completed_tasks] }
     { employee_id: employee_id, completed_tasks: employee_data[:completed_tasks] }
