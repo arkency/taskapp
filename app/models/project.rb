3a2b46121d54392ecf6cac37c1fa9811a538c7e1
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-07-23 17:29:35 +0200

    We have to tell about this as well, Tomek
    ```
      Project.new(name: "HEJ").save!
    ```
    
    UNFORTUNATELY, it is possible to broadcast changes from after commits
    and other hooks :(

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  after_create_commit do
+    broadcast_prepend_to "projects", target: :projects_table_body, partial: "projects/table_row", locals: { project: self }
+  end
 end
