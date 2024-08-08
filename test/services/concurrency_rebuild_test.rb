0d9cce737abe4145bf64364b21ea625f51b7a767
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-08-08 10:43:42 +0200

    Adjust test scenario
    TaskCompleted should be appended after the task_stream of thread 1
    is fetched
    
    Co-authored-by: Tomasz Stolarczyk <tomasz.stolarczyk@arkency.com>

--- test/services/concurrency_rebuild_test.rb
+++ test/services/concurrency_rebuild_test.rb
@@ -4,27 +4,36 @@ require_relative "../test_helper"
 require "database_cleaner/active_record"
 
 class ControlledTaskViewModelBuilder < TaskViewModelBuilder
-  def initialize(exchanger)
+  def initialize(exchanger, thread)
     @exchanger = exchanger
+    @thread = thread
   end
 
   def change_task_name(event, task)
+    p "=== Thread #{@thread} before name exchange"
+    @exchanger.exchange!(:message)
+    p "=== Thread #{@thread} after name exchange"
     @exchanger.exchange!(:message)
     super
+
+    @exchanger.exchange!(:message)
   end
 
   def complete_task(_event, task)
     super
+
+    p "=== Thread #{@thread} before complete exchange"
+
     @exchanger.exchange!(:message)
+
+    p "=== Thread #{@thread} after complete exchange"
   end
 end
 
 class ConcurrencyBetweenRebuildingTaskViewReadModelsTest < ActiveSupport::TestCase
   self.use_transactional_tests = false
 
-  setup do
-    DatabaseCleaner.strategy = :truncation
-  end
+  setup { DatabaseCleaner.strategy = :truncation }
 
   def test_concurrent_replay_of_events_and_processing_new_event__expected_that_the_event_is_processed_when_replay_is_finished
     begin
@@ -32,13 +41,28 @@ class ConcurrencyBetweenRebuildingTaskViewReadModelsTest < ActiveSupport::TestCa
       assert concurrency_level >= 4
 
       task_id = SecureRandom.uuid
-      event_store.append(TaskCreated.new(data: { task_id: task_id }), stream_name: "Task$#{task_id}")
+      event_store.append(
+        TaskCreated.new(data: { task_id: task_id }),
+        stream_name: "Task$#{task_id}"
+      )
 
       100.times do |i|
-        event_store.append(TaskDateAssigned.new(data: { task_id: task_id, due_date: Date.current + i.day }), stream_name: "Task$#{task_id}")
+        event_store.append(
+          TaskDateAssigned.new(
+            data: {
+              task_id: task_id,
+              due_date: Date.current + i.day
+            }
+          ),
+          stream_name: "Task$#{task_id}"
+        )
       end
-      task_name_changed_newer = TaskNameChanged.new(data: { task_id: task_id, name: "Init name" })
-      event_store.append(task_name_changed_newer, stream_name: "Task$#{task_id}")
+      task_name_changed_newer =
+        TaskNameChanged.new(data: { task_id: task_id, name: "Init name" })
+      event_store.append(
+        task_name_changed_newer,
+        stream_name: "Task$#{task_id}"
+      )
       last_applied_event = nil
 
       TaskViewModelBuilder.new.call(task_name_changed_newer)
@@ -48,13 +72,21 @@ class ConcurrencyBetweenRebuildingTaskViewReadModelsTest < ActiveSupport::TestCa
 
       threads = [
         Thread.new do
-          ControlledTaskViewModelBuilder.new(exchanger).rebuild(task_id)
+          ControlledTaskViewModelBuilder.new(exchanger, 1).rebuild(task_id)
         end,
         Thread.new do
           last_applied_event = TaskCompleted.new(data: { task_id: task_id })
-          event_store.append(last_applied_event, stream_name: "Task$#{task_id}")
+
+          p "=== Thread 2 before append exchange"
           exchanger.exchange(:message)
-          ControlledTaskViewModelBuilder.new(exchanger).call(last_applied_event)
+
+          event_store.append(last_applied_event, stream_name: "Task$#{task_id}")
+
+          p "=== Thread 2 after append exchange"
+          exchanger.exchange(:message)
+          ControlledTaskViewModelBuilder.new(exchanger, 2).call(
+            last_applied_event
+          )
         end
       ]
 
